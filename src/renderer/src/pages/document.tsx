ebb84b62b89373f56cf1cdc84d8112f434b53145
ssisaias <ssisaias@users.noreply.github.com>
2023-10-03 15:39:09 -0300

    initial implementation -> doc page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,23 @@
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Document
-      <Link to="/">Acessar blank</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-bold text-xs">
+          TABLE OF CONTENTS
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back End</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Dbs</ToC.Link>
+            <ToC.Link>Auth</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">Section</section>
     </main>
   )
 }
