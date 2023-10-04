15d1e647c0b878f937a8ea7e43766d893b07d5a3
ssisaias <ssisaias@users.noreply.github.com>
2023-10-04 09:54:55 -0300

    Adds support to tsconfig paths for vite

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,15 +2,20 @@ import { resolve } from 'node:path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import tsconfigPathsPlugin from 'vite-tsconfig-paths'
+
+const tsconfigPaths = tsconfigPathsPlugin({
+  projects: [resolve('tsconfig.json')],
+})
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
 
     publicDir: resolve('resources'),
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [tsconfigPaths, externalizeDepsPlugin()],
   },
   renderer: {
     define: {
@@ -30,6 +35,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src'),
       },
     },
-    plugins: [react()],
+    plugins: [tsconfigPaths, react()],
   },
 })
