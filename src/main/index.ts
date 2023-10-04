1662b4fe025c5fb7e09b1e754e1690484831ef71
ssisaias <ssisaias@users.noreply.github.com>
2023-10-04 09:46:01 -0300

    Set up electron-store
    electron-store is usually used for configs and not DB cases,
    we are using it for now for learning purposes only

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,7 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import { createFileRoute, createURLRoute } from 'electron-router-dom'
 
 import './ipc'
+import './store'
 
 function createWindow(): void {
   // Create the browser window.
---
+++ src/main/store.ts
@@ -0,0 +1,15 @@
+import Store from 'electron-store'
+
+interface StoreType {
+  documents: Record<string, any>
+}
+
+export const store = new Store<StoreType>({
+  defaults: {
+    documents: {},
+  },
+})
+
+
+
+console.log(store.path)
